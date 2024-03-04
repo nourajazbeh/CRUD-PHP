abd8766a050c065ac59d5f0dd7657ce44e2bf5e4
Noura Jazbeh <dr.noorj19@gmail.com>
2024-03-04 12:18:20 +0100

    adding bootstrap ,und toster.js

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <?php
+include_once('db.php');
+$action = false;
 
-if(isset($_POST['save'])){
+if(isset($_POST['save'])) {
   $name = $_POST['name'];
   $email = $_POST['email'];
   $mobile = $_POST['mobile'];
   $password = $_POST['password'];
   
-  $add_sql="INSERT INTO 'users'( 'name','email', 'password', 'mobile') VALUES ('$name','$email','$password','$mobile')";
-  
-}
+  $add_sql = "INSERT INTO 'users'( 'name','email', 'password', 'mobile') VALUES
+  ('$name','$email','$password','$mobile')";
+  $res_add =mysqli_query($con, $add_sql);
+  if(!$res_add) {
+    die(mysqli_error($con));
 
-?>
+  }else {
+    $action = "add";
+  }
+
+
+  }
+  $add_sql="SELECT * FROM users";
+  $res_add =mysqli_query($con, $add_sql);
+
+  var_dump
+
+
+?>?
 
 <!DOCTYPE html>
 
@@ -20,6 +36,8 @@ if(isset($_POST['save'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/toster.css">
+
     <title>user</title>
 </head>
 
@@ -27,22 +45,55 @@ if(isset($_POST['save'])){
     <div class="container">
       <div class="wrapper p-5 m-5">
         <div class="d-flex p-2 justify-contevt-between">
-        <div class="d-flex p-2 justify-content-between mb-2">
            <h2>All users</h2>
            <div><a href="add_user.php"><i data-feather="user-plus"></i></a></div>
 
-
         </div>
-
-      </div> 
+        <table class="table table-striped">
+         <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Mobile</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>  
+         </table>
+        </div> 
      
-    </div>
-    APP
-    <i data-feather="user-plus"></i>
-    <script src="js/bootstrap.bundle.min.js"></script>
+      </div>
+      <script src="js/jg.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script src="js/icons.js"></script>
+    <script src="js/toster.js"></script>
+    <script src="js/main.js"></script>
+    <?php
+    
+    if($ection == 'del') { ?>
+      <script>
+        show_del()
+      </script>
+
+
+      <?php
+    }
+    if ($action == 'edit') { ?>
+      <script>
+        show_update()
+      </script>
+
+
+      <?php
+    }
+  
+
+  ?>
     <script>
-        feather.replace();
+      feather.replace();
+
+
+      toaster.info("Are?")
     </script>
 </body>
 
